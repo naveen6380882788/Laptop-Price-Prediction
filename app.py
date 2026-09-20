3b7a1a3e8ed2b7d72b6fe9b6bf86550afe0d7963
naveen6380882788 <naveenrajeswaran466@gmail.com>
2026-09-20 11:35:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -243,30 +243,6 @@ if st.button("🔮 Predict Laptop Price"):
 
 
 # =========================
-# DATASET INFO
-# =========================
-st.markdown(
-    """
-    <div class="info-box">
-        <h3>📊 Dataset Information</h3>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.metric("Records", len(df))
-
-with col2:
-    st.metric("Features", 4)
-
-with col3:
-    st.metric("Target", "Price")
-
-
-# =========================
 # FOOTER
 # =========================
 st.markdown(
